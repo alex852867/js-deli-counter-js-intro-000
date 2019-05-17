06a76436968adc5d5f59934d9785475ed3341595
Alex East <alexeastltd@gmail.com>
2019-05-17 11:18:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,20 +4,15 @@ function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name);
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
 }
-
-
-
-function nowServing (katzDeliLine) {
-if (katzDeliLine.length > 1) {
-var customer = katzDeliLine[0];
-katzDeliLine.shift();
-return `Now serving ${customer}`
-} else {
-return 'There is nobody waiting to be served!'
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length > 1) {
+    var custName = katzDeliLine[0];
+    katzDeliLine.shift();
+    return `Currently serving ${custName}.`;
+  } else {
+    return `There is nobody waiting to be served!`;
+  }
 }
-}
-
-
 
 var line = []
 function currentLine(katzDeliLine) {
