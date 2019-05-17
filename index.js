929d8e548762677a0ac93a5cec9853ac0d8d733d
Alex East <alexeastltd@gmail.com>
2019-05-17 10:26:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,20 +3,3 @@ function takeANumber(katzDeliLine, name) {
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
 
-function nowServing(katzDeliline) {
-  if (katzDeliline.length ===0) {
-  return 'There is nobody waiting to be served!'
-} else {
-  return `Currently serving ${katzDeliLine.shift()}.`
-}
-}
-function currentLine(katzDeliLine) {
-  for (var i = 0; i < katzDeliLine.length; i++) {
-    line.push (`${i+1}. ${line[i]}`)
-}
-      if (katzDeliLinel.length > 1) {
-    return `The line is currently: ${line}`
-  } else {
-    return 'The line is currently empty.'
-  }
-  }
