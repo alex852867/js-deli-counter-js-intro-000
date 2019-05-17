cf84a7678d3a5d8f224a3bd875621179ccb904e8
Alex East <alexeastltd@gmail.com>
2019-05-17 10:05:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,10 @@ function nowServing(line) {
 }
 }
 function currentLine(line) {
-  if (line.length ===0) {
+  for (var i = 0; i < line.length; i++) {
+    line.push (`${i}. ${line}`) 
+
+      if (line.length ===0) {
     return 'The line is currently empty.'
   } else{
     return `The line is currently: 1.${line=0}. 2.${line=1}`
