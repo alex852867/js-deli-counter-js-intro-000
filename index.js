8432ab061c71ccd9d9f8b4bc7042f13d55837944
Alex East <alexeastltd@gmail.com>
2019-05-17 11:08:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function takeANumber(katzDeliLine, name) {
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
 }
 function nowServing (katzDeliLine) {
-if (katzDeliLine < 1) {
+if (katzDeliLine > 1) {
 var customer = katzDeliLine[0];
 katzDeliLine.upshift();
 return `Now serving ${customer}`
