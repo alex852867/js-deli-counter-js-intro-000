4aefee616dde5f22d9b13fd852f3a63a22d5cd39
Alex East <alexeastltd@gmail.com>
2019-05-17 10:24:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
-function takeANumber(line, name) {
-  line.push(name)
-  return `Welcome, ${name}. You are number ${line.length} in line.`
+function takeANumber(katzDeliLine, name) {
+  katzDeliline.push(name)
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
 
-function nowServing(line) {
-  if (line.length ===0) {
+function nowServing(katzDeliline) {
+  if (katzDeliline.length ===0) {
   return 'There is nobody waiting to be served!'
 } else {
-  return `Currently serving ${line.shift()}.`
+  return `Currently serving ${katzDeliLine.shift()}.`
 }
 }
-function currentLine(line) {
-  for (var i = 0; i < line.length; i++) {
-    line.push (`${i}. ${line}`)
-
-      if (line.length ===0) {
+function currentLine(katzDeliLine) {
+  for (var i = 0; i < katzDeliLine.length; i++) {
+    line.push (`${i+1}. ${line[i]}`)
+}
+      if (katzDeliLinel.length > 1) {
+    return {`The line is currently: ${line}`
+  } else {
     return 'The line is currently empty.'
-  } else{
-    return `The line is currently: 1.${line=0}. 2.${line=1}`
   }
   }
+  
