0b2925c6190f213e53fc685407eb5442eea0934a
Alex East <alexeastltd@gmail.com>
2019-05-17 10:27:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,14 @@ function takeANumber(katzDeliLine, name) {
   katzDeliline.push(name)
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
+
+function currentLine(katzDeliLine) {
+  for (var i = 0; i < katzDeliLine.length; i++) {
+    line.push (`${i+1}. ${line[i]}`)
+}
+      if (katzDeliLinel.length > 1) {
+    return `The line is currently: ${line}`
+  } else {
+    return 'The line is currently empty.'
+  }
+  }
