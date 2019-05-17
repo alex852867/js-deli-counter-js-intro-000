2a2fc60f38535e65fa25ab3b42105e69f97411c9
Alex East <alexeastltd@gmail.com>
2019-05-17 10:41:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name);
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
 }
+var line = []
 function currentLine(katzDeliLine) {
   for (var i = 0; i < katzDeliLine.length; i++) {
     line.push (`${i+1}. ${katzDeliLine[i]}`)
